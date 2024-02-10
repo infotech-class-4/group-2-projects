a759f2b531b1ede1cab2f1605d7c89ab38380a8c
ozlemakcicek <ozlemakcicek83@gmail.com>
2024-02-10 23:53:43 +0100

    card created

--- DOM-Project/app.js
+++ DOM-Project/app.js
@@ -1,79 +1,141 @@
 
+const select = document.querySelector("#users");
+
+const container = document.querySelector(".container");
+
+const getUsers = () => {
+  return axios.get("https://jsonplaceholder.typicode.com/users");
+};
+
+let allUser = [];
+
+window.addEventListener("load", async () => {
+  const { data } = await getUsers();
+  allUser = data;
+  console.log(data);
+
+  data.forEach((user) => {
+    //console.log(user.name);
+    const option = document.createElement("option");
+
+    option.value = user.id;
+    option.textContent = user.name;
+
+    select.appendChild(option);
+  });
+});
+
+// fotograflar
+
+const getPhotos = (userId) => {
+  return axios.get(
+    `https://jsonplaceholder.typicode.com/albums/${userId}/photos`
+  );
+};
 
 
-const select=document.querySelector("#users")
+// post
 
-const getUsers=()=>{
-    return axios.get("https://jsonplaceholder.typicode.com/users");
-}
+const getPosts = (userId) => {
+  return axios.get(
+    `https://jsonplaceholder.typicode.com/posts/?userId=${userId}`
+  );
+};
 
-window.addEventListener("load",async ()=>{
+select.addEventListener("change", async (e) => {
+  //console.log(e.target.value);
 
-    const {data}= await getUsers()
+  const getPhoto = await getPhotos(e.target.value);
 
-    console.log(data);
+  const getPost = await getPosts(e.target.value);
+  console.log(getPost);
 
-    data.forEach((user)=>{
+  createCard(getPhoto.data);
+});
 
-        //console.log(user.name);
-const option=document.createElement("option")
-option.value=user.id
-option.textContent=user.name
-
-
-select.appendChild(option)
-    })
-
-})
-
-
-
-select.addEventListener("change",async (e)=>{
-
-   //console.log(e.target.value); 
-
-   const {data}=await getPhotos(e.target.value)
-   console.log(data);
-
-
-
-})
-
-
-const getPhotos= (userId)=>{
-    return axios.get(`https://jsonplaceholder.typicode.com/albums/${userId}/photos`);
-}
-
-/*
-
-<div class="col-lg-6">
-<div class="card mb-4">
-    <div class="card-body">
-       
-          
-                <h5 class="mt-0">Top-aligned media</h5>
-                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-                <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-             
-                <div class="media">
-            <img class="align-self-start mr-3" src="..." alt="Generic placeholder image">
-            <div class="media-body">
-             </div>
-            </div> 
-
-    </div>
-    <div class="card-footer">
-        FOOTER KISMI
-    </div>
-</div>
-</div>
-
-*/
 
 
 // Card yapisi
 
 
 
+const createCard = (photos) => {
+  console.log(photos);
+
+  photos.forEach((photo) => {
+    const row = document.createElement("div");
+
+    row.innerHTML = `
+        <div class="col-lg-6">
+          <div class="card mb-4">
+            <div class="card-body">
+              <div class="media">
+                <div class="imgli d-flex justify-content-between">
+                 
+                  <img
+                    id="img"
+                    class="align-self-start mr-3 rounded-circle"
+                    src=${photo.thumbnailUrl}
+                    alt="Generic placeholder image"
+                  />
+                  <label for="" class="label"> label gelecek</label>| Bret
+
+                  <i class="fa-regular fa-trash-can del"></i>
+                </div>
+                <div class="media-body">
+                  <img src=${photo.url} alt="" class="img-body" /> 
+
+                  <div>
+                    burayada post dan cekilen verilerin body parametsei gelcek
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer d-flex justify-content-between">
+            <div>
+            <i class="fa-solid fa-thumbs-up like"  onclick="likeTik()" onmouseover="likeHover()" onmouseout="likeHoverOut()" ></i>
+              <span id="up">0</span>
+              </div>
+              <div>
+              <i class="fa-solid fa-thumbs-down dislike" style="color:red"></i>
+              <span id="down">0</span>
+              </div>
+              
+
+              <div> 
+              <i class="fa-solid fa-comment comment" style="color:pink"></i>
+              <span id="comment">0</span>
+              </div>
+
+           
+            </div>
+          </div>
+        </div>`;
+
+    container.append(row);
+  });
+};
+
+let countLike = 0;
+const likeTik = () => {
+  console.log("tiklandi");
+
+  const likeSpan = document.querySelector("#up");
+  likeSpan.textContent = ++countLike;
+};
+
+const likeHover = () => {
+  const like = document.querySelector(".like");
+  like.style.color = "red";
+};
+
+const likeHoverOut = () => {
+  const like = document.querySelector(".like");
+  like.style.color = "black";
+};
 
 
+
+
+
+   
